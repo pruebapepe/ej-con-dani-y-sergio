eed26fe03536e5d13db12401cd952827296baf9c
alvaro <alvaro@172.16.20.116>
2024-03-18 20:21:12 +0100

    version 1 de mi calculadora ( añadir potencia de nuemros)

---
+++ pruebaMain/src/pruebaMain/MainCalculadora.java
@@ -0,0 +1,30 @@
+package pruebaMain;
+
+import java.util.Scanner;
+
+
+public class MainCalculadora {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sn = new Scanner(System.in);
+		
+		System.out.println("ingresa el primer numero");
+		int n1 = sn.nextInt();
+		
+		System.out.println("ingresa el segundo numero");
+		int n2 = sn.nextInt();
+		
+		calculadora calc = new calculadora();
+		
+		calc.sumar(n1,n2);
+		calc.restar(n1,n2);
+		calc.multiplicar(n1,n2);
+		calc.dividir(n1,n2);
+		
+		calc.resultado();
+		
+	}
+
+}
---
+++ pruebaMain/src/pruebaMain/calculadora.java
@@ -0,0 +1,85 @@
+package pruebaMain;
+
+public class calculadora {
+	
+	private int suma;
+	private int resta;
+	private int division;
+	private int multiplicacion;
+	
+
+
+	public int getSuma() {
+		return suma;
+	}
+
+
+	public void setSuma(int suma) {
+		this.suma = suma;
+	}
+
+
+	public int getResta() {
+		return resta;
+	}
+
+
+	public void setResta(int resta) {
+		this.resta = resta;
+	}
+
+
+	public int getDivision() {
+		return division;
+	}
+
+
+	public void setDivision(int division) {
+		this.division = division;
+	}
+
+
+	public int getMultiplicacion() {
+		return multiplicacion;
+	}
+
+
+	public void setMultiplicacion(int multiplicacion) {
+		this.multiplicacion = multiplicacion;
+	}
+	
+	
+	
+	
+	public void sumar(int numero1, int numero2){
+		
+		suma = numero1 + numero2;
+	}
+	
+	public void restar(int numero1, int numero2){
+		
+		resta = numero1 - numero2;
+		
+	}
+	
+	public void multiplicar(int numero1, int numero2) {
+		
+		multiplicacion = numero1 * numero2;
+		
+	}
+	
+	public void dividir(int numero1, int numero2) {
+		
+		division = numero1 / numero2;
+		
+	}
+	
+
+	public void resultado() {
+		System.out.println("la suma es :"+suma);
+		System.out.println("la resta es :"+resta);
+		System.out.println("la multiplicacion es :"+multiplicacion);
+		System.out.println("la division es :"+division);
+		
+	}
+}
